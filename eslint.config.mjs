884c421fbe99f21640e40f717020232061ce00c0
Yliu <yangboses@gmail.com>
2024-09-22 13:34:25 +0800

    feat: 初始化项目结构

---
+++ eslint.config.mjs
@@ -0,0 +1,59 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import checkFile from 'eslint-plugin-check-file';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    // in main config for TSX/JSX source files
+    plugins: {
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      'react-refresh/only-export-components': 'warn',
+    },
+  },
+  {
+    files: ['packages/**/*'],
+    plugins: {
+      'check-file': checkFile,
+    },
+    rules: {
+      'check-file/no-index': 'error',
+
+      'check-file/folder-match-with-fex': [
+        'error',
+        {
+          '*.test.{js,jsx,ts,tsx}': '**/__tests__/',
+          '*.styled.{jsx,tsx}': '**/pages/',
+        },
+      ],
+      'check-file/filename-naming-convention': [
+        'error',
+        {
+          '**/src/**/*.{jsx,tsx}': 'CAMEL_CASE',
+          '**/src/**/*.{js,ts}': 'KEBAB_CASE',
+        },
+      ],
+      'check-file/folder-naming-convention': [
+        'error',
+        {
+          '**/src/**/': 'CAMEL_CASE',
+          // 'mocks/*/': 'KEBAB_CASE',
+        },
+      ],
+    },
+  },
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+];
---
+++ packages/interface/rsbuild.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginLess } from '@rsbuild/plugin-less';
+
+export default defineConfig({
+  plugins: [pluginReact(), pluginLess()],
+  source: {
+    entry: {
+      index: './src/main.tsx',
+    },
+  },
+  html: {
+    title: `图床资源管理`,
+    favicon: './src/assets/CatppuccinFolderImagesOpen.svg',
+  },
+});
