206f824b01e0118b4be9dc719b192d0214edd457
yliu <yangboses@gmail.com>
2024-09-26 15:13:23 +0800

    feat: 客户端初始化

---
+++ packages/interface-electron/src/entry.js
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+
+const { register } = require('esbuild-register/dist/node');
+
+register();
+
+require('./main.ts');
---
+++ packages/interface-electron/src/main.ts
@@ -0,0 +1,22 @@
+import { app, BrowserWindow } from 'electron';
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadURL(`http://localhost:4444/`);
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
